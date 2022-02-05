088cbd4b3ed7cfd3a3458883a6b8150f70291570
alfabar <alfa_bar@hotmail.com>
2022-02-05 08:35:56 -0300

    atualizado

--- quiz/models.py
+++ quiz/models.py
@@ -1,11 +1,30 @@
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
 
 class Quizzes(models.Model):
+    class Meta:
+        verbose_name = _("Quiz")
+        verbose_name_plural = _("Quizzes")
+        ordering = ['id']
+    category = models.ForeignKey(
+        Category, default=1, on_delete=models.DO_NOTHING)
     pass
 
 class Question(UpdatedQuestio):
+    quiz = models.ForeignKey(
+        Quizzes, related_name='question', on_delete=models.DO_NOTHING)
+    pass
+
+class Answer(UpdatedQuestion):
+    question = models.ForeignKey(
+        Question, related_name='answer', on_delete=models.DO_NOTHING)
     pass
