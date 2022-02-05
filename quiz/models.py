26143ea17b0df31b8812f287f0e84ba06d203552
alfabar <alfa_bar@hotmail.com>
2022-02-05 08:23:49 -0300

    atualizado

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    pass
+
+class Quizzes(models.Model):
+    pass
+
+class Question(UpdatedQuestio):
+    pass
